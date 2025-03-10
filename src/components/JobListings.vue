7ce6a921c66b1aa07dac82289bc12d7c307c4f8d
Andrei Reyes <rchristianandrei@gmail.com>
2025-03-11 04:31:48 +0800

    add icons

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -23,11 +23,11 @@ const jobs = ref<Job[]>(jobsRepo.getJobs())
         <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mx-2 lg:mx-auto my-5 max-w-[1000px]">
             <Card v-for="(job, index) in jobs.slice(0, limit)" :key="index" class="bg-white rounded p-5 my-5">
                 <div class="text-gray-500">{{job.type}}</div>
-                <h2 class="text-xl font-bold">{{job.title}}</h2>
+                <h2 class="text-xl font-bold"><span class="pi pi-briefcase"></span> {{job.title}}</h2>
                 <p class="my-3 text-justify">{{job.description}}</p>
                 <div class="text-green-500 font-medium my-1"><span>{{job.salary}}</span> / Month</div>
                 <div class="h-[1px] bg-gray-400"></div>
-                <div class="text-red-700 font-semibold my-1">{{job.location}}</div>
+                <div class="text-red-700 font-semibold my-1"><span class="pi pi-map-marker"></span> {{job.location}}</div>
                 <button type="button" class="cursor-pointer text-white font-medium bg-green-500 p-2 rounded w-[100%] hover:opacity-[0.5]">Read More</button>
             </Card>
         </div>
